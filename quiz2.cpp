4563cf72822dfd8d551a1f4a36ac24b452e62521
Isha Chakraborty <isha.chakraborty@utexas.edu>
2020-04-06 22:21:07 -0500

    More edits to Quiz2

--- quiz2.cpp
+++ quiz2.cpp
@@ -1,24 +1,26 @@
 #include <cstdint>
-#include <assert.h>
-#include <stdio.h>
 
-bool sumsTo(uint32_t x[], uint32_t n, uint32_t k, uint32_t v){
+bool subsetSum(uint32_t x[], uint32_t n, uint32_t k, uint32_t v){
     // Base cases
-    if (k == 0 && v == 0){
+    if (k == 0 && v == 0){ // when you've added k terms that add up to v
         return 1;
-    } if (k == 0 && v != 0){
+    } if (k == 0 && v != 0){ // when you've added k terms that do not add up to v
         return 0;
-    } if (k > n || n == 0){ // extra base case, where you run out of terms to add while k > 0
+    } if (n == 0){ // when you run out of terms to add while k > 0
         return 0;
     }
 
-    bool case1 = sumsTo(x + 1, n - 1, k, v); // sum does not include x[0]
-    bool case2 = 0;
-    if (v >= x[0]){
-        case2 = sumsTo(x + 1, n - 1, k - 1, v - x[0]); // sum includes x[0]
-    }
-    if (case1 || case2){
+    bool case1 = 0;
+    if (n > k) // if n <= k, x[0] must be included in sum, so case1 isn't even evaluated
+        case1 = subsetSum(x + 1, n - 1, k, v); // sum does not include x[0]
+    if (case1) // if case 1 == true, no need to check case 2
         return 1;
-    }
+
+    bool case2 = 0;
+    if (v >= x[0]) // shouldn't include x[0] in sum if it is bigger than v
+        case2 = subsetSum(x + 1, n - 1, k - 1, v - x[0]); // sum includes x[0]
+    if (case2)
+        return 1;
+
     return 0;
 }
